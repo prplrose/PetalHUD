aa3a687134c4641b1fcd102f3477c35d28d59341
prplrose <158543118+prplrose@users.noreply.github.com>
2024-02-22 22:22:57 +0100

    Initial commit

---
+++ src/main/java/com/github/prplrose/petalhud/PetalHUD.java
@@ -0,0 +1,13 @@
+package com.github.prplrose.petalhud;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PetalHUD implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/prplrose/petalhud/client/PetalHUDClient.java
@@ -0,0 +1,11 @@
+package com.github.prplrose.petalhud.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PetalHUDClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
